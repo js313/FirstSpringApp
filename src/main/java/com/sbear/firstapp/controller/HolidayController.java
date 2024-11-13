84baed0f0668431a99787cd0794d6d20710a1c63
alexander <alexander@pop.os>
2024-11-13 20:35:17 +0530

    request params and path variables

--- src/main/java/com/sbear/firstapp/controller/HolidayController.java
+++ src/main/java/com/sbear/firstapp/controller/HolidayController.java
@@ -5,7 +5,10 @@ import com.sbear.firstapp.service.HolidayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,6 +16,9 @@ import java.util.stream.Collectors;
 @Controller
 public class HolidayController {
     HolidayService holidayService;
+    public enum DisplayType {
+        all, federal, festival
+    }
 
     @Autowired
     HolidayController(HolidayService holidayService) {
@@ -20,12 +26,33 @@ public class HolidayController {
     }
 
     @RequestMapping("/holidays")
-    public String DisplayHolidays(Model model) {
+    public String displayHolidays(Model model, @RequestParam(required = false) boolean festival, @RequestParam(required = false) boolean federal) {
+        model.addAttribute("federal", federal);
+        model.addAttribute("festival", festival);
         List<Holiday> holidays = holidayService.getHolidayList();
 
         Holiday.Type[] types = Holiday.Type.values();
 
         for (Holiday.Type type : types) {
+            if((type == Holiday.Type.FEDERAL && !federal) || (type == Holiday.Type.FESTIVAL && !festival)) continue;
+            model.addAttribute(type.toString(),
+                    (holidays.stream().filter(holiday -> holiday.type().equals(type)).collect(Collectors.toList())));
+        }
+
+        return "holidays.html";
+    }
+
+    @GetMapping("/holidays/{display}")
+    public String displayHolidays(Model model, @PathVariable DisplayType display) {
+        model.addAttribute("federal", (display == DisplayType.all || display == DisplayType.federal));
+        model.addAttribute("festival", (display == DisplayType.all || display == DisplayType.festival));
+        List<Holiday> holidays = holidayService.getHolidayList();
+
+        Holiday.Type[] types = Holiday.Type.values();
+
+        for (Holiday.Type type : types) {
+            if((type == Holiday.Type.FEDERAL && !(display == DisplayType.all || display == DisplayType.federal)) ||
+                    (type == Holiday.Type.FESTIVAL && !(display == DisplayType.all || display == DisplayType.festival))) continue;
             model.addAttribute(type.toString(),
                     (holidays.stream().filter(holiday -> holiday.type().equals(type)).collect(Collectors.toList())));
         }
