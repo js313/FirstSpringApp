2b36f1b18fe0f6ecfbd6b44c44b06c90438edf8c
alexander <alexander@pop.os>
2024-11-13 14:23:35 +0530

    add lombok library to reduce boilerplate

--- src/main/java/com/sbear/firstapp/service/ContactService.java
+++ src/main/java/com/sbear/firstapp/service/ContactService.java
@@ -1,14 +1,12 @@
 package com.sbear.firstapp.service;
 
 import com.sbear.firstapp.model.Contact;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.logging.Logger;
-
+@Slf4j
 @Service
 public class ContactService {
-    private static final Logger log = Logger.getLogger(ContactService.class.getName());
-
     public void saveMessageDetails(Contact contact) {
         log.info(contact.toString());
     }
