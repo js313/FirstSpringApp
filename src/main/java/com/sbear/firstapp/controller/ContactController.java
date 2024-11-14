19c18457d312ad1966064d3fb05cf677a0ec1954
alexander <alexander@pop.os>
2024-11-14 23:44:13 +0530

    validations

--- src/main/java/com/sbear/firstapp/controller/ContactController.java
+++ src/main/java/com/sbear/firstapp/controller/ContactController.java
@@ -2,12 +2,17 @@ package com.sbear.firstapp.controller;
 
 import com.sbear.firstapp.model.Contact;
 import com.sbear.firstapp.service.ContactService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 public class ContactController {
 
@@ -19,7 +24,8 @@ public class ContactController {
     }
 
     @RequestMapping("/contact")
-    public String displayContactPage() {
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact", new Contact());   // Add a new Contact object to the model
         return "contact.html";
     }
 
@@ -33,8 +39,12 @@ public class ContactController {
 //        return new ModelAndView("redirect:/contact");
 //    }
     @PostMapping("/saveMsg")
-    public ModelAndView saveMessage(Contact contact) {
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors) {   // @ModelAttribute("contact") annotation binds the form data to the Contact object
+        if(errors.hasErrors()) {
+            log.error("Contact form validation failed due to: {}", errors.toString());
+            return "contact.html";
+        }
         contactService.saveMessageDetails(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 }
--- src/main/java/com/sbear/firstapp/model/Contact.java
+++ src/main/java/com/sbear/firstapp/model/Contact.java
@@ -1,13 +1,31 @@
 package com.sbear.firstapp.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Contact {
     // Need to be named same as the html form names
+    @NotBlank(message="Name must not be blank")
+    @Size(min=3, message = "Name must be at least 3 characters long")
     private String name;
+
+    @NotBlank(message="Mobile Number must not be blank")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 DIGITS")
     private String mobileNum;
+
+    @NotBlank(message="Email must not be blank")
+    @Email(message = "Email is not valid")
     private String email;
+
+    @NotBlank(message="Subject must not be blank")
+    @Size(min = 5, message = "Subject must be at least 5 characters")
     private String subject;
+
+    @NotBlank(message="Message must not be blank")
+    @Size(min = 10, max = 100, message = "Subject must be between 10 - 100 characters long")
     private String message;
 }
